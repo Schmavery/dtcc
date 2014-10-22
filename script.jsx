a3a438d5b2d7df271da9c63fcb3d02a167f36530
Benjamin San Souci <benjamin.sansouci@gmail.com>
2014-10-22 15:04:04 -0400

    rebuilt in react

---
+++ script.jsx
@@ -0,0 +1,53 @@
+/** @jsx React.DOM */
+
+var Main = React.createClass({
+  getInitialState: function() {
+    return {
+      style: {
+        position: "absolute",
+        top: window.innerHeight / 2 - 150,
+        left: window.innerWidth / 2 - 167,
+        width: 334,
+        height: 300,
+        textAlign: "center"
+      },
+      showAnswer: false
+    };
+  },
+  render: function() {
+    var answer;
+    if(this.state.showAnswer) {
+      answer = (
+        <div>
+          You asked if &#39;{this.refs.textinput.getDOMNode().value}&#39; causes cancer.
+          <br/><br/>
+          YES.
+        </div>
+      );
+    }
+    return (
+      <div id="main" style={this.state.style}>
+        <h1>
+        Does this causes cancer?
+        </h1>
+        <form onSubmit={this.submit}>
+        <input type="text" ref="textinput" onKeyUp={this.onTextChange} style={{width: "100%"}}/>
+        <button onClick={this.submit}>Search</button>
+        <button onClick={this.submit}>I&#39;m feeling lucky</button>
+        </form>
+        <br />
+        {answer}
+      </div>
+    );
+  },
+
+  submit: function(e) {
+    e.preventDefault();
+    this.setState({
+      showAnswer: true
+    });
+    return false;
+  }
+});
+
+React.renderComponent(<Main />, document.getElementById("react"));
